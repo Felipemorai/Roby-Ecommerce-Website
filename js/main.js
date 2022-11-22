9cebf1b30fec8a5b1eff1941abe8d6c8552e5e06
Luiz Felipe <felipemorai188@gmail.com>
2022-11-22 15:38:10 -0300

    onscroll

--- js/main.js
+++ js/main.js
@@ -42,3 +42,14 @@ function scrollActive() {
         }
     })
 }
+
+/* Change Color Header */
+window.onscroll = () => {
+    const nav = document.getElementById('header');
+
+    if (this.scrollY >= 200) {
+        nav.classList.add('scroll-header'); 
+    } else {
+        nav.classList.remove('scroll-header');
+    }
+}
